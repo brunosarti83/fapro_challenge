738454e5f975ab9f92402c023f8d17b35356918c
brunosarti83 <brunosarti.bs@gmail.com>
2024-08-18 15:55:56 -0300

    [deploy]csrf

--- faproscraper/settings.py
+++ faproscraper/settings.py
@@ -144,5 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-909c.up.railway.app', 'http://web-production-909c.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://*', 'http://*']
 CORS_ALLOW_ALL_ORIGINS = True
